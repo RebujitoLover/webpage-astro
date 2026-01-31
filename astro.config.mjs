3106c1c8487be59e2b2c43a143a93687172e6fe4
German <0611396916@uma.es>
2026-01-31 18:43:17 +0100

    Finished tutorial and implemented content collections

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
-export default defineConfig({ site: 'https://app.netlify.com/projects/german-torres-tendero/' });
+export default defineConfig({
+  site: 'https://app.netlify.com/projects/german-torres-tendero/',
+  integrations: [preact()]
+});
